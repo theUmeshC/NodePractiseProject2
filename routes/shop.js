9ea915ae8c810e0ca86a283958c2e8db2fd6d364
theUmeshC <umesh.c@celestialsys.com>
2022-12-02 16:29:05 +0530

    course practise project

--- routes/shop.js
+++ routes/shop.js
@@ -1,3 +1,5 @@
+const path = require('path');
+
 const express = require('express');
 
 const shopController = require('../controllers/shop');
@@ -8,8 +10,12 @@ router.get('/', shopController.getIndex);
 
 router.get('/products', shopController.getProducts);
 
+router.get('/products/:productId', shopController.getProduct);
+
 router.get('/cart', shopController.getCart);
 
-router.get('/checkout', shopController.getCheckout);
+router.post('/cart', shopController.postCart);
+
+router.post('/cart-delete-item', shopController.postCartDeleteProduct);
 
 module.exports = router;
